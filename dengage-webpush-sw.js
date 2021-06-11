82b122246c30984987d9caee17a5f6d482b801a5
Sergen Tufanoğulları <sergen@whitehorse.technology>
2021-06-11 14:56:54 +0300

    fix: web push

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -2,5 +2,5 @@ var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
 if (accountId && appGuid) {
-    importScripts("https://test-dengage-publicsignal.apps.nonprod.ebt.bank/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+    importScripts("https://public-data-collector.burgan.com.tr/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
 }
