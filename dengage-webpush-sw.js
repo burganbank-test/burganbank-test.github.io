a18369e46db6a6e623b30db836327dfa49480f33
Sergen Tufanoğulları <sergen@whitehorse.technology>
2021-07-27 15:26:57 +0300

    fix: change service worker

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -2,5 +2,5 @@ var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
 if (accountId && appGuid) {
-    importScripts("https://public-data-collector.burgan.com.tr/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+    importScripts("https://dengage-public-data-collector-proxy.burgan.com.tr/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
 }
